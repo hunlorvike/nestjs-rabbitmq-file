7ca77e98ce9f63aa83485c9975ec82cb097c87fd
nguyen viet hung <hungcutedethuongg@gmail.com>
2024-02-15 17:18:17 +0700

    Fix lỗi dùng swagger không upload được file

--- src/modules/upload/dtos/file-upload.dto.ts
+++ src/modules/upload/dtos/file-upload.dto.ts
@@ -1,12 +1,10 @@
 // file-upload.dto.ts
 
 import { ApiProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
 import { IsNotEmpty } from 'class-validator';
-import { Express } from 'express';
 
 export class FileUploadDto {
-    @ApiProperty({ type: 'string', format: 'binary' })
-    @IsNotEmpty()
-    file: Express.Multer.File;
+  @ApiProperty({ type: 'string', format: 'binary' })
+  @IsNotEmpty()
+  file: Express.Multer.File;
 }
--- src/modules/upload/file-upload.controller.ts
+++ src/modules/upload/file-upload.controller.ts
@@ -1,10 +1,11 @@
 // file-upload.controller.ts
 
-import { Controller, Inject, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Inject, Post, UseInterceptors, UploadedFile, Body, HttpException, HttpStatus } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { Services } from '../../shared/constrains/constrain';
 import { FileUploadService } from './file-upload.service';
+import { FileUploadDto } from './dtos/file-upload.dto';
 
 @Controller('files')
 @ApiTags('files')
@@ -16,12 +17,22 @@ export class FileUploadController {
     @Post('upload')
     @UseInterceptors(FileInterceptor('file'))
     @ApiConsumes('multipart/form-data')
+    @ApiBody({
+        description: 'File upload',
+        type: FileUploadDto,
+    })
     async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<{ data: string }> {
-        if (!file) {
-            return { data: 'File not provided' };
-        }
+        try {
+            console.log(file);
 
-        const result = await this.uploadService.uploadFile(file);
-        return { data: result };
+            if (!file) {
+                throw new HttpException('File not provided', HttpStatus.BAD_REQUEST);
+            }
+
+            const result = await this.uploadService.uploadFile(file);
+            return { data: result };
+        } catch (error) {
+            throw new HttpException(error.message || 'Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
